cb253fa2c5dc1187ac7cc5444065e6f37e767558
jkiddd <ivan_x_@mail.ru>
2019-08-15 13:33:13 +0600

    оператор переключатель

---
+++ lesson5.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Домашнее задание</title>
+</head>
+<body>
+
+<?php
+#swithc
+$day = 25;
+
+switch ($day) {
+
+   case 1: 
+      echo 'это рабочий день';
+      break;
+   case 2: 
+      echo 'это рабочий день';
+      break;
+   case 3: 
+      echo 'это рабочий день';
+      break;
+   case 4: 
+      echo 'это рабочий день';
+      break;
+   case 5: 
+      echo 'это рабочий день';
+      break;
+   case 6: 
+      echo 'это выходной день';
+      break;
+   case 7: 
+      echo 'это выходной день';
+      break;
+   default:
+      echo 'неизвестный день';
+
+}
+
+
+
+?>
+</body>
+</html>
